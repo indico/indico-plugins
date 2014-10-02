fbd01a99a56691bbf733969c54c32e42ec453ae1
Alejandro Avilés <aavilesd@cern.ch>
2014-10-02 17:52:37 +0200

    Add piwik-specific settings to form

--- piwik/indico_piwik/forms.py
+++ piwik/indico_piwik/forms.py
@@ -1,8 +1,16 @@
-from wtforms import BooleanField, IntegerField
+from wtforms import BooleanField, IntegerField, StringField
 
 from indico.web.forms.base import IndicoForm
+from MaKaC.i18n import _
 
 
 class SettingsForm(IndicoForm):
-    cache_enabled = BooleanField('Enable report caching', default=True)
-    cache_ttl = IntegerField('Report caching TTL (seconds)', default=3600)
+    cache_enabled = BooleanField(_("Enable report caching"), default=True)
+    cache_ttl = IntegerField(_("Report caching TTL (seconds)"), default=3600)
+    js_hook_enabled = BooleanField(_("Enable conference and contribution view tracking"), default=True)
+    download_tracking_enabled = BooleanField(_("Enable material download tracking"), default=True)
+    server_url = StringField(_("Piwik general server URL (piwik.php)"), default='127.0.0.1/piwik/')
+    server_api_url = StringField(_("Piwik API server URL (index.php)"), default='127.0.0.1/piwik/')
+    user_only_server_url = BooleanField(_("Use only the general URL for all requests"), default=True)
+    server_token = StringField(_("Piwik API token"))
+    server_site_id = StringField(_("Piwik site ID"), default='1')
