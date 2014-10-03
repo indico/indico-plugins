a6a88dde9e9f5f867386901f4892043ac3cb64bd
Pedro Ferreira <pedro.ferreira@cern.ch>
2014-10-03 16:28:10 +0200

    Added site-wide tracking
    Hooks on to page-header template hook from core

--- piwik/indico_piwik/__init__.py
+++ piwik/indico_piwik/__init__.py
@@ -1,3 +1,5 @@
+from flask_pluginengine import render_plugin_template
+
 from indico.core import signals
 from indico.core.plugins import IndicoPlugin, IndicoPluginBlueprint, url_for_plugin
 from MaKaC.i18n import _
@@ -19,6 +21,7 @@ class PiwikPlugin(IndicoPlugin):
     def init(self):
         super(PiwikPlugin, self).init()
         self.connect(signals.event_management_sidemenu, self.add_sidemenu_item)
+        self.template_hook('page-header', self.inject_page_header)
 
     def add_sidemenu_item(self, event):
         menu_item = SideMenuItem(_("Piwik Statistics"), url_for_plugin('piwik.view', event))
@@ -32,6 +35,16 @@ class PiwikPlugin(IndicoPlugin):
         self.register_css_bundle('statistics_css', 'css/statistics.css')
         self.register_js_bundle('jqtree_js', 'js/lib/jqTree/tree.jquery.js')
         self.register_css_bundle('jqtree_css', 'js/lib/jqTree/jqtree.css')
+
+    def inject_page_header(self, template, **kwargs):
+        server_url = self.settings.get('server_url')
+
+        if not self.settings.get('enabled') or not server_url:
+            return ""
+        else:
+            return render_plugin_template('site_tracking.html',
+                                          site_id=self.settings.get('site_id_general'),
+                                          server_url=server_url)
 
 
 blueprint = IndicoPluginBlueprint('piwik', __name__)
