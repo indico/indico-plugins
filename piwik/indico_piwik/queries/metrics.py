a9bc9f629811d548675a693f4a750208acccf303
Alejandro Avilés <aavilesd@cern.ch>
2014-10-10 14:40:47 +0200

    Add metric query for downloads

--- piwik/indico_piwik/queries/metrics.py
+++ piwik/indico_piwik/queries/metrics.py
@@ -1,6 +1,7 @@
 from __future__ import division
 
 from operator import attrgetter, itemgetter
+from urllib2 import quote
 
 from . import PiwikQueryReportEventBase
 from .utils import get_json_from_remote_server, reduce_json, stringify_seconds
@@ -18,6 +19,45 @@ class PiwikQueryReportEventMetricBase(PiwikQueryReportEventBase):
         if not result:
             return 0
         return result if return_json else int(reduce_json(result))
+
+
+class PiwikQueryReportEventMetricDownloads(PiwikQueryReportEventMetricBase):
+    def call(self, download_url):
+        return super(PiwikQueryReportEventMetricReferrers, self).call(method='Actions.getDownload',
+                                                                      downloadUrl=quote(download_url))
+
+    def get_result(self, download_url):
+        result = get_json_from_remote_server(self.call, download_url=download_url)
+        return {'cumulative': self._get_cumulative_results(result),
+                'individual': self._get_per_day_results(result)}
+
+    def _get_per_day_results(self, results):
+        hits_calendar = {}
+
+        # Piwik returns hits as a list of hits per date.
+        for date, hits in results.iteritems():
+            day_hits = {'total_hits': 0, 'unique_hits': 0}
+            if hits:
+                for metrics in hits:
+                    day_hits['total_hits'] += metrics['nb_hits']
+                    day_hits['unique_hits'] += metrics['nb_uniq_visitors']
+            hits_calendar[date] = day_hits
+
+        return hits_calendar
+
+    def _get_cumulative_results(self, results):
+        """
+        Returns a dictionary of {'total_hits': x, 'unique_hits': y} for the
+        date range.
+        """
+        total_hits = {'total_hits': 0, 'unique_hits': 0}
+
+        day_hits = list(hits[0] for hits in results.values() if hits)
+        for metrics in day_hits:
+            total_hits['total_hits'] += metrics['nb_hits']
+            total_hits['unique_hits'] += metrics['nb_uniq_visitors']
+
+        return total_hits
 
 
 class PiwikQueryReportEventMetricReferrers(PiwikQueryReportEventMetricBase):
