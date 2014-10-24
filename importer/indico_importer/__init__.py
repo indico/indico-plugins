5962abb84bb82c56a11583f7a2f23b0ec72c96a6
Alejandro Avilés <aavilesd@cern.ch>
2014-10-24 14:42:59 +0200

    Add static assets

--- importer/indico_importer/__init__.py
+++ importer/indico_importer/__init__.py
@@ -1,5 +1,22 @@
-from indico.core.plugins import IndicoPlugin
+from indico.core.plugins import IndicoPlugin, IndicoPluginBlueprint
+from MaKaC.webinterface.pages.conferences import WPConfModifScheduleGraphic
 
 
-class IndicoImporter(IndicoPlugin):
-    pass
+class ImporterPlugin(IndicoPlugin):
+    """Importer plugin
+
+    Extends Indico for other plugins to import data from external sources to
+    the timetable.
+    """
+
+    def init(self):
+        super(ImporterPlugin, self).init()
+        self.inject_js('importer_js', WPConfModifScheduleGraphic)
+        self.inject_css('importer_css', WPConfModifScheduleGraphic)
+
+    def get_blueprints(self):
+        return IndicoPluginBlueprint('importer', __name__)
+
+    def register_assets(self):
+        self.register_js_bundle('importer_js', 'js/importer.js')
+        self.register_css_bundle('importer_css', 'css/importer.css')
