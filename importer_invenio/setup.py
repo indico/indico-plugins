914479f9e44e18d737b3696694946e4c3b78acbe
Alejandro Avilés <aavilesd@cern.ch>
2014-10-27 16:41:24 +0100

    Initial commit

---
+++ importer_invenio/setup.py
@@ -0,0 +1,34 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2014 European Organization for Nuclear Research (CERN).
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Indico is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Indico; if not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='indico_importer_invenio',
+    version='0.1',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'indico>=1.9.1',
+        'indico_importer'
+    ],
+    entry_points={'indico.plugins': {'importer_invenio = indico_importer_invenio:ImporterInvenioPlugin'}}
+)
