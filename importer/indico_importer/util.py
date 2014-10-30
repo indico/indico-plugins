fee6b45d35f1e5497fd5ede2ca5960376aad000c
Alejandro Avilés <aavilesd@cern.ch>
2014-10-30 10:23:25 +0100

    Rename module and method

--- importer/indico_importer/util.py
+++ importer/indico_importer/util.py
@@ -17,11 +17,11 @@
 from __future__ import unicode_literals
 
 
-def parse_datetime(string):
-    split_datetime = string[0].split('T')
+def convert_dt_tuple(dt_tuple):
+    split_datetime = dt_tuple[0].split('T')
     if len(split_datetime) > 1:
-        return {'date': string[0].split('T')[0],
-                'time': string[0].split('T')[1]}
+        return {'date': dt_tuple[0].split('T')[0],
+                'time': dt_tuple[0].split('T')[1]}
     else:
-        return {'date': string[0].split('T')[0],
+        return {'date': dt_tuple[0].split('T')[0],
                 'time': '00:00'}
