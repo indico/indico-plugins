bf55f0d0a27138cc0ab98b5e64107153c078720b
Adrian Moennich <adrian.moennich@cern.ch>
2015-01-05 11:20:30 +0100

    Payment/PayPal: use unicode_literals, remove space

--- payment_paypal/indico_payment_paypal/controllers.py
+++ payment_paypal/indico_payment_paypal/controllers.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Indico; if not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import unicode_literals
+
 from itertools import chain
 from urllib import urlencode
 from urllib2 import urlopen
