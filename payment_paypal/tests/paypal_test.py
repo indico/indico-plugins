a2ce28326383747cee0621938e17229d54d97747
Adrian Moennich <adrian.moennich@cern.ch>
2015-01-21 10:35:56 +0100

    Payment/PayPal: Test IPN RH

--- payment_paypal/tests/paypal_test.py
+++ payment_paypal/tests/paypal_test.py
@@ -18,6 +18,8 @@ import pytest
 from flask import request
 from mock import MagicMock
 
+from indico.modules.payment.models.transactions import PaymentTransaction
+
 from indico_payment_paypal.controllers import RHPaypalIPN
 from indico_payment_paypal.plugin import PaypalPaymentPlugin
 
@@ -25,9 +27,9 @@ from indico_payment_paypal.plugin import PaypalPaymentPlugin
 @pytest.mark.usefixtures('db', 'request_context')
 @pytest.mark.parametrize(('business', 'expected'), (
     ('test', True),
-    ('foo', False)
+    ('foo',  False)
 ))
-def test_verify_business(business, expected):
+def test_ipn_verify_business(business, expected):
     rh = RHPaypalIPN()
     rh.event = MagicMock(id=1)
     PaypalPaymentPlugin.event_settings.set(rh.event, 'business', 'test')
@@ -41,7 +43,7 @@ def test_verify_business(business, expected):
     ('13.37', True),
     ('10.00', False)
 ))
-def test_verify_amount(mocker, amount, expected):
+def test_ipn_verify_amount(mocker, amount, expected):
     nai = mocker.patch('indico_payment_paypal.controllers.notify_amount_inconsistency')
     rh = RHPaypalIPN()
     rh.event = MagicMock(id=1)
@@ -51,3 +53,54 @@ def test_verify_amount(mocker, amount, expected):
     with PaypalPaymentPlugin.instance.plugin_context():
         assert rh._verify_amount() == expected
         assert nai.called == (not expected)
+
+
+@pytest.mark.usefixtures('request_context')
+@pytest.mark.parametrize(('txn_id', 'payment_status', 'expected'), (
+    ('12345',  'Completed', True),
+    ('12345',  'Pending',   False),
+    ('123456', 'Completed', False),
+    ('123456', 'Pending',   False),
+))
+def test_ipn_is_transaction_duplicated(mocker, txn_id, payment_status, expected):
+    flfr = mocker.patch('indico_payment_paypal.controllers.PaymentTransaction.find_latest_for_registrant')
+    request.form = {'payment_status': 'Completed', 'txn_id': '12345'}
+    rh = RHPaypalIPN()
+    rh.registrant = MagicMock()
+    flfr.return_value = None
+    assert not rh._is_transaction_duplicated()
+    flfr.return_value = PaymentTransaction(provider='paypal', data={'payment_status': payment_status, 'txn_id': txn_id})
+    assert rh._is_transaction_duplicated() == expected
+
+
+@pytest.mark.usefixtures('db', 'request_context')
+@pytest.mark.parametrize('fail', (
+    'business',
+    'verify',
+    'dup_txn',
+    'fail',
+    'refund',
+    'negative',
+    'status',
+    None,
+))
+def test_ipn_process(mocker, fail):
+    rt = mocker.patch('indico_payment_paypal.controllers.register_transaction')
+    post = mocker.patch('indico_payment_paypal.controllers.requests.post')
+    post.return_value.text = 'INVALID' if fail == 'verify' else 'VERIFIED'
+    rh = RHPaypalIPN()
+    rh._verify_business = lambda: fail != 'business'
+    rh._is_transaction_duplicated = lambda: fail == 'dup_txn'
+    rh.event = MagicMock(id=1)
+    rh.registrant = MagicMock()
+    rh.registrant.getTotal.return_value = 10.00
+    payment_status = {'fail': 'Failed', 'refund': 'Refunded', 'status': 'Foobar'}.get(fail, 'Completed')
+    amount = '-10.00' if fail == 'negative' else '10.00'
+    request.view_args = {'confId': rh.event.id}
+    request.args = {'registrantId': '1'}
+    request.form = {'payment_status': payment_status, 'txn_id': '12345', 'mc_gross': amount,
+                    'mc_currency': 'EUR'}
+    with PaypalPaymentPlugin.instance.plugin_context():
+        rh._process()
+        assert post.called == (fail != 'business')
+        assert rt.called == (fail is None)
