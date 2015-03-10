818e7aa90c6f2fdcd7eb65e0de041256a1ecb9fe
Adrian Moennich <adrian.moennich@cern.ch>
2015-03-10 17:14:52 +0100

    VC/Vidyo: Fix intermediate alembic steps

--- vc_vidyo/indico_vc_vidyo/migrations/201502191158_a702ad0bcea_create_extensions_table.py
+++ vc_vidyo/indico_vc_vidyo/migrations/201502191158_a702ad0bcea_create_extensions_table.py
@@ -20,10 +20,13 @@ def upgrade():
     op.execute(CreateSchema('plugin_vc_vidyo'))
     op.create_table('vidyo_extensions',
                     sa.Column('vc_room_id', sa.Integer(), nullable=False),
-                    sa.Column('extension', sa.BigInteger(), nullable=True, index=True),
-                    sa.Column('owned_by_id', sa.Integer(), nullable=True, index=True),
-                    sa.ForeignKeyConstraint(['vc_room_id'], ['events.vc_rooms.id']),
-                    sa.PrimaryKeyConstraint('vc_room_id'),
+                    sa.Column('extension', sa.BigInteger(), nullable=True),
+                    sa.Column('owned_by_id', sa.Integer(), nullable=True),
+                    sa.ForeignKeyConstraint(['vc_room_id'], ['events.vc_rooms.id'],
+                                            name='vidyo_extensions_vc_room_id_fkey'),
+                    sa.PrimaryKeyConstraint('vc_room_id', name='vidyo_extensions_pkey'),
+                    sa.Index('ix_plugin_vc_vidyo_vidyo_extensions_extension', 'extension'),
+                    sa.Index('ix_plugin_vc_vidyo_vidyo_extensions_owned_by_id', 'owned_by_id'),
                     schema='plugin_vc_vidyo')
 
 
