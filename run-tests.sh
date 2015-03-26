05a63adbf519d64d8751317b7de4bb674551c114
Jacques Dafflon <jacques.dafflon@cern.ch>
2015-03-26 18:33:47 +0100

    Small fix to the run-test.sh script

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-for dir in $(find -name pytest.ini -exec dirname {} +); do
+for dir in $(find -name pytest.ini -exec dirname {} \;); do
     pushd "$dir" >/dev/null
     py.test "$@"
     popd >/dev/null
