b16a368ba14067bc372229e0bc65b3b119b5aaff
Indico Team <indico-team@cern.ch>
2016-01-01 04:59:45 +0100

    Happy new year 2016!
    Bump license headers to 2016

--- vc_vidyo/indico_vc_vidyo/blueprint.py
+++ vc_vidyo/indico_vc_vidyo/blueprint.py
@@ -1,5 +1,5 @@
 # This file is part of Indico.
-# Copyright (C) 2002 - 2015 European Organization for Nuclear Research (CERN).
+# Copyright (C) 2002 - 2016 European Organization for Nuclear Research (CERN).
 #
 # Indico is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -12,7 +12,7 @@
 # General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Indico; if not, see <http://www.gnu.org/licenses/>.from __future__ import unicode_literals
+# along with Indico; if not, see <http://www.gnu.org/licenses/>.
 
 from indico.core.plugins import IndicoPluginBlueprint
 
