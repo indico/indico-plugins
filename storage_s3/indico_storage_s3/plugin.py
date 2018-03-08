eaf32f115310f11fb756c874eed3eae92ef43183
Natalia Juszka <natalia.karina.juszka@cern.ch>
2018-03-08 16:37:21 +0100

    Storage/S3: create plugin

---
+++ storage_s3/indico_storage_s3/plugin.py
@@ -0,0 +1,62 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2018 European Organization for Nuclear Research (CERN).
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Indico is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Indico; if not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+import ast
+
+from indico.core.plugins import IndicoPlugin
+from indico.core.storage import Storage
+from indico.core import signals
+
+
+class S3StoragePlugin(IndicoPlugin):
+    """S3 Storage
+
+    Provides S3 storage backends.
+    """
+
+    def init(self):
+        super(S3StoragePlugin, self).init()
+        self.connect(signals.get_storage_backends, self._get_storage_backends)
+
+    def _get_storage_backends(self, sender, **kwargs):
+        return S3Storage
+
+
+class S3Storage(Storage):
+    name = 's3'
+    simple_data = False
+
+    def __init__(self, data):
+        data = self._parse_data(data)
+        self.could_host = data['host']
+        self.datestamp = bool(ast.literal_eval(data.get('datestamp', 'False').title()))
+
+    def open(self, file_id):
+        pass
+
+    def save(self, file_id):
+        pass
+
+    def delete(self, file_id):
+        pass
+
+    def getsize(self, file_id):
+        pass
+
+    def send_file(self, file_id):
+        pass
