67e1dfd2195f3ac2fa7df5dc0215897d0773088c
Adrian Moennich <adrian.moennich@cern.ch>
2018-05-14 15:13:48 +0200

    Update plugin versions & meta package

--- chat/setup.py
+++ chat/setup.py
@@ -21,7 +21,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='indico-plugin-chat',
-    version='2.0.dev0',
+    version='2.0',
     description='XMPP chat integration for Indico',
     url='https://github.com/indico/indico-plugins',
     license='https://www.gnu.org/licenses/gpl-3.0.txt',
