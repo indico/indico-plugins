90050ae8ae28e1733240280174729fa3f10c59d5
Adrian Moennich <adrian.moennich@cern.ch>
2018-11-01 17:15:49 +0100

    Storage/S3: Avoid unnecessary indentation

--- storage_s3/indico_storage_s3/task.py
+++ storage_s3/indico_storage_s3/task.py
@@ -33,17 +33,18 @@ from plugin import S3Storage
 def create_bucket():
     for key in config.STORAGE_BACKENDS:
         storage = get_storage(key)
-        if isinstance(storage, S3Storage):
-            today = date.today()
-            bucket_name = storage._get_original_bucket_name()
-            placeholders = set(re.findall('<.*?>', bucket_name))
-            if placeholders == {'<year>', '<week>'}:
-                bucket_date = today + relativedelta(weeks=1)
-                bucket = storage._replace_bucket_placeholders(bucket_name, bucket_date)
-                storage._create_bucket(bucket)
-            elif placeholders == {'<year>', '<month>'} or (placeholders == {'<year>'} and today.month == 12):
-                bucket_date = today + relativedelta(months=1)
-                bucket = storage._replace_bucket_placeholders(bucket_name, bucket_date)
-                storage._create_bucket(bucket)
-            elif placeholders == {'<month>'} or placeholders == {'<week>'} or placeholders == {'<month>', '<week>'}:
-                raise RuntimeError('Placeholders combination in bucket name is not correct: %s', bucket_name)
+        if not isinstance(storage, S3Storage):
+            continue
+        today = date.today()
+        bucket_name = storage._get_original_bucket_name()
+        placeholders = set(re.findall('<.*?>', bucket_name))
+        if placeholders == {'<year>', '<week>'}:
+            bucket_date = today + relativedelta(weeks=1)
+            bucket = storage._replace_bucket_placeholders(bucket_name, bucket_date)
+            storage._create_bucket(bucket)
+        elif placeholders == {'<year>', '<month>'} or (placeholders == {'<year>'} and today.month == 12):
+            bucket_date = today + relativedelta(months=1)
+            bucket = storage._replace_bucket_placeholders(bucket_name, bucket_date)
+            storage._create_bucket(bucket)
+        elif placeholders == {'<month>'} or placeholders == {'<week>'} or placeholders == {'<month>', '<week>'}:
+            raise RuntimeError('Placeholders combination in bucket name is not correct: %s', bucket_name)
