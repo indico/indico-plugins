2b3525931ff0781f8c3d36d07ae938c6c503429d
penelopec <penelope@fnal.gov>
2018-11-06 13:09:06 -0600

    Fixed code according to comments

--- payment_paypal/indico_payment_paypal/plugin.py
+++ payment_paypal/indico_payment_paypal/plugin.py
@@ -38,12 +38,17 @@ class PluginSettingsForm(PaymentPluginSettingsFormBase):
     business = StringField(_('Business'), [Optional(), validate_business],
                            description=_('The default PayPal ID or email address associated with a PayPal account. '
                                          'Event managers will be able to override this.'))
-    itemize = BooleanField(_('Itemize PayPal Cart'), [Optional()], widget=SwitchWidget(), description=_('Check to itemize the PayPal cart'))
+    itemize = BooleanField(_('Itemize PayPal Cart'), [Optional()], widget=SwitchWidget(), 
+                           description=_('Check to itemize the PayPal cart'))
+
 
 class EventSettingsForm(PaymentEventSettingsFormBase):
-    business = StringField(_('Business'), [UsedIf(lambda form, _: form.enabled.data), DataRequired(), validate_business],
+    business = StringField(_('Business'), [UsedIf(lambda form, _: form.enabled.data), DataRequired(), 
+                                           validate_business],
                            description=_('The PayPal ID or email address associated with a PayPal account.'))
-    itemize = BooleanField(_('Itemize PayPal Cart'), [Optional()], widget=SwitchWidget(), description=_('Check to itemize the PayPal cart'))
+    itemize = BooleanField(_('Itemize PayPal Cart'), [Optional()], widget=SwitchWidget(), 
+                           description=_('Check to itemize the PayPal cart'))
+
 
 class PaypalPaymentPlugin(PaymentPluginMixin, IndicoPlugin):
     """PayPal
@@ -75,93 +80,70 @@ class PaypalPaymentPlugin(PaymentPluginMixin, IndicoPlugin):
             unicode_to_ascii(remove_accents(registration.full_name, reencode=False)),
             unicode_to_ascii(remove_accents(event.title, reencode=False))
         )
+        data['return_url'] = url_for_plugin('payment_paypal.success', registration.locator.uuid, _external=True)
+        data['cancel_url'] = url_for_plugin('payment_paypal.cancel', registration.locator.uuid, _external=True)
+        data['notify_url'] = url_for_plugin('payment_paypal.notify', registration.locator.uuid, _external=True)
 
-# ##################################################
-# PayPal Itemized cart get the billable items
+# PayPal Itemized cart get all billable items
         itemized_data = []
-        ii = 1
-        paypal_data = ""
         if registration.base_price:
-            choice = {}
-            choice['title']       = 'Registration Fee'
-            choice['price']       = format_currency(registration.base_price, '', u'#0.00', locale='en_US') 
-            choice['quantity']    = 1
-            choice['price_total'] = choice['price']
-            choice['price_info']  = registration.base_price_info
-            choice['itemNum']     = ii
+            choice = {
+                      'title': 'Registration Fee'.
+                      'price': format_currency(registration.base_price, '', u'#0.00', locale='en_US'),
+                      'quantity': 1,
+                      'price_info': registration.base_price_info
+                     }
             itemized_data.append(choice)
 
-        for section, fields in registration.summary_data.iteritems():	# if not section.is_manager_only:
-            sectionTitle = section.title
-            
-            for field, regdata in fields.iteritems():	# if regdata.friendly_data:
+        for section, fields in registration.summary_data.iteritems():            
+            for field, regdata in fields.iteritems():
                 friendly_data  = regdata.friendly_data
                 versioned_data = regdata.field_data.versioned_data
                 unversioned_data = regdata.field_data.field.data
-                fieldTitle = field.title
             
-                choice = {}
                 if regdata.field_data.field.input_type == 'accommodation' and friendly_data:
-                    billable_choices = [x for x in versioned_data['choices'] if x['id'] in regdata.data['choice']  and x['is_billable']]
+                    billable_choices = [x for x in versioned_data['choices'] if x['id'] in regdata.data['choice'] and x['is_billable']]
                     if billable_choices:
                         if not friendly_data['is_no_accommodation']:
-                            ii = ii + 1
-                            choice = {}
-                            choice['title']       = fieldTitle + ' - ' + friendly_data['choice']
-                            choice['price']       = format_currency(billable_choices[0]['price'], '', u'#0.00', locale='en_US') 
-                            choice['quantity']    = friendly_data['nights']
-                            choice['price_total'] = regdata.price
-                            choice['price_info']  = billable_choices[0]['price_info']
-                            choice['itemNum']     = ii
+                            choice = {
+                                      'title': '{} - {}'.format(section.title, friendly_data['choice']),
+                                      'price': format_currency(billable_choices[0]['price'], '', u'#0.00', locale='en_US'),
+                                      'quantity': friendly_data['nights'],
+                                      'price_info': billable_choices[0]['price_info'] 
+                                     }
                             itemized_data.append(choice)
 
                 elif regdata.field_data.field.input_type == 'single_choice':
                     billable_choices = [x for x in versioned_data['choices'] if x['id'] in regdata.data and x['is_billable']]
                     if billable_choices:
-                        ii = ii + 1
-                        choice = {}
-                        choice['title']       = fieldTitle + ' - ' + unversioned_data['captions'][billable_choices[0]['id']]
-                        choice['price']       = format_currency(billable_choices[0]['price'], '', u'#0.00', locale='en_US') 
-                        choice['quantity']    = regdata.data[billable_choices[0]['id']]
-                        choice['price_total'] = regdata.price
-                        choice['price_info']  = billable_choices[0]['price_info']
-                        choice['itemNum']     = ii
+                        choice = {
+                                  'title': '{} - {}'.format(field.title, unversioned_data['captions'][billable_choices[0]['id']]), 
+                                  'price': format_currency(billable_choices[0]['price'], '', u'#0.00', locale='en_US'), 
+                                  'quantity': regdata.data[billable_choices[0]['id']],
+                                  'price_info': billable_choices[0]['price_info']
+                                 }
                         itemized_data.append(choice)
                  
                 elif regdata.field_data.field.input_type == 'multi_choice':
                     billable_choices = [x for x in versioned_data['choices'] if x['id'] in regdata.data and x['is_billable']]
                     if billable_choices:
-                        for bc in billable_choices:                            
-                            ii = ii + 1
-                            choice = {}
-                            choice['title']       = fieldTitle + ' - ' + unversioned_data['captions'][bc['id']]
-                            choice['price']       = format_currency(bc['price'], '', u'#0.00', locale='en_US') 
-                            choice['quantity']    = regdata.data[bc['id']]
-                            choice['price_total'] = bc['price'] * regdata.data[bc['id']]
-                            choice['price_info']  = bc['price_info']
-                            choice['itemNum']     = ii
+                        for bc in billable_choices:  
+                            choice = {
+                                      'title': '{} - {}'.format(field.title, unversioned_data['captions'][bc['id']]),
+                                      'price': format_currency(bc['price'], '', u'#0.00', locale='en_US'),
+                                      'quantity': regdata.data[bc['id']],
+                                      'price_info': bc['price_info']
+                                     }
                             itemized_data.append(choice)
 
                 else: 
-                    if regdata.price:                        
-                        ii = ii + 1
-                        choice = {}
-                        choice['title']       = sectionTitle + ' - ' + fieldTitle
-                        choice['price']       = format_currency(versioned_data['price'], '', u'#0.00', locale='en_US') 
-                        if regdata.field_data.field.input_type == 'number':
-                            choice['quantity'] = regdata.data
-                        else:
-                            choice['quantity'] = 1
-                        choice['price_total'] = regdata.price
-                        choice['price_info']  = versioned_data['price_info']
-                        choice['itemNum']     = ii
+                    if regdata.price: 
+                        choice = {
+                                  'title': '{} - {}'.format(section.title, field.title),
+                                  'price': format_currency(versioned_data['price'], '', u'#0.00', locale='en_US'),
+                                  'quantity': regdata.data if regdata.field_data.field.input_type == 'number' else 1,
+                                  'price_info': versioned_data['price_info']
+                                 }
                         itemized_data.append(choice)
 
         data['itemized_data'] = itemized_data
-# ################################################## 
-
-        data['return_url'] = url_for_plugin('payment_paypal.success', registration.locator.uuid, _external=True)
-        data['cancel_url'] = url_for_plugin('payment_paypal.cancel', registration.locator.uuid, _external=True)
-        data['notify_url'] = url_for_plugin('payment_paypal.notify', registration.locator.uuid, _external=True)
-
-
