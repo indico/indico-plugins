d2a5aadd9bfa1b3c521ea3cbebca7561164ac627
Adrian Moennich <adrian.moennich@cern.ch>
2018-11-08 17:54:26 +0100

    Storage/S3: Remove -dev from version

--- storage_s3/setup.py
+++ storage_s3/setup.py
@@ -21,7 +21,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='indico-plugin-storage-s3',
-    version='2.0-dev',
+    version='2.0',
     description='S3 storage backend for Indico',
     url='https://github.com/indico/indico-plugins',
     license='https://www.gnu.org/licenses/gpl-3.0.txt',
