bd50c6c89ad470af6dad9f5c26c2879f17da8bee
Wibowo Arindrarto <bow@bow.web.id>
2019-01-30 11:03:43 +0100

    Add missing url_prefix to blueprint

--- indico_payment_stripe/blueprint.py
+++ indico_payment_stripe/blueprint.py
@@ -13,6 +13,13 @@ from indico.core.plugins import IndicoPluginBlueprint
 from indico_payment_stripe.controllers import RHStripe
 
 
-blueprint = IndicoPluginBlueprint('payment_stripe', __name__)
+blueprint = IndicoPluginBlueprint(
+    'payment_stripe',
+    __name__,
+    url_prefix=(
+        '/event/<confId>/registrations/'
+        '<int:reg_form_id>/payment/response/stripe'
+    )
+)
 
 blueprint.add_url_rule('/handler', 'handler', RHStripe, methods=['POST'])
