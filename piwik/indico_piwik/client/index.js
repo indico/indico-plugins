ac7b813e4619e1a89c75864654c2107896236505
Indico Team <indico-team@cern.ch>
2019-05-08 11:16:13 +0200

    Update license headers (and add missing)

--- piwik/indico_piwik/client/index.js
+++ piwik/indico_piwik/client/index.js
@@ -1,3 +1,10 @@
+// This file is part of the Indico plugins.
+// Copyright (C) 2002 - 2019 CERN
+//
+// The Indico plugins are free software; you can redistribute
+// them and/or modify them under the terms of the MIT License;
+// see the LICENSE file for more details.
+
 import 'jqtree';
 import 'jquery';
 import './main.css';
