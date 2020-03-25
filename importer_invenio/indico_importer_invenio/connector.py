687e95cc905c7d5c1e8e2e6d351896f21d740556
Adrian Moennich <adrian.moennich@cern.ch>
2020-03-25 15:51:56 +0100

    Update flake8 config

--- importer_invenio/indico_importer_invenio/connector.py
+++ importer_invenio/indico_importer_invenio/connector.py
@@ -5,6 +5,8 @@
 # them and/or modify them under the terms of the MIT License;
 # see the LICENSE file for more details.
 
+# flake8: noqa
+
 """
 Tools to connect to distant Invenio servers using Invenio APIs.
 
