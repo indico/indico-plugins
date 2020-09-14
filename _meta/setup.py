463fe4e61fa61e569fdfd4a30851b16a48702899
Adrian Moennich <adrian.moennich@cern.ch>
2020-09-14 17:33:08 +0200

    meta: Bump version for 2.3

--- _meta/setup.py
+++ _meta/setup.py
@@ -13,32 +13,33 @@ from setuptools import setup
 # Do not modify this block manually - use `update-meta.py` instead!
 # BEGIN GENERATED REQUIREMENTS
 plugins_require = [
-    'indico-plugin-importer==2.2',
-    'indico-plugin-importer-invenio==2.2',
-    'indico-plugin-livesync==2.0',
-    'indico-plugin-payment-manual==1.0.1',
-    'indico-plugin-payment-paypal==2.2',
-    'indico-plugin-piwik==2.2',
-    'indico-plugin-previewer-code==1.0',
-    'indico-plugin-previewer-jupyter==1.0',
-    'indico-plugin-search==2.2',
-    'indico-plugin-storage-s3==2.0.3',
-    'indico-plugin-vc-vidyo==2.2',
+    'indico-plugin-livesync>=2.3,<2.4.dev0',
+    'indico-plugin-payment-manual>=1.0.1,<2.4.dev0',
+    'indico-plugin-payment-paypal>=2.2,<2.4.dev0',
+    'indico-plugin-piwik>=2.3,<2.4.dev0',
+    'indico-plugin-previewer-code>=1.0,<2.4.dev0',
+    'indico-plugin-previewer-jupyter>=1.0,<2.4.dev0',
+    'indico-plugin-search>=2.3,<2.4.dev0',
+    'indico-plugin-storage-s3>=2.3,<2.4.dev0',
+    'indico-plugin-ursh>=2.3,<2.4.dev0',
+    'indico-plugin-vc-vidyo>=2.3,<2.4.dev0',
 ]
-extras_require = {}
+extras_require = {
+    'importer': ['indico-plugin-importer-invenio>=2.2,<2.4.dev0', 'indico-plugin-importer>=2.2,<2.4.dev0'],
+}
 # END GENERATED REQUIREMENTS
 
 
 setup(
     name='indico-plugins',
-    version='2.2.1',
+    version='2.3',
     description='A meta-package containing the official Indico plugins',
     url='https://github.com/indico/indico-plugins',
     license='MIT',
     author='Indico Team',
     author_email='indico-team@cern.ch',
     zip_safe=False,
-    install_requires=['indico>=2.2.dev0'] + plugins_require,
+    install_requires=['indico>=2.3.dev0'] + plugins_require,
     extras_require=extras_require,
     classifiers=[
         'Environment :: Plugins',
