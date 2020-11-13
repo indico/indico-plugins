5436b29767cfafd77f66c8791f4857de61eba71c
Pedro Ferreira <pedro.ferreira@cern.ch>
2020-11-13 10:23:17 +0100

    VC/Zoom: set correct version, setup.py improvements

--- vc_zoom/setup.py
+++ vc_zoom/setup.py
@@ -5,14 +5,15 @@ from setuptools import find_packages, setup
 
 setup(
     name='indico-plugin-vc-zoom',
-    version='0.3-dev',
+    version='2.3-dev',
     description='Zoom video-conferencing plugin for Indico',
-    url='',
+    url='https://github.com/indico/indico-plugins',
     license='MIT',
-    author='Giovanni Mariano - ENEA',
-    author_email='giovanni.mariano@enea.it',
+    author='Giovanni Mariano (ENEA) and Indico Team (CERN)',
+    author_email='indico-team@cern.ch',
     packages=find_packages(),
     zip_safe=False,
+    platforms='any',
     include_package_data=True,
     install_requires=[
         'indico>=2.3.2.dev0',
