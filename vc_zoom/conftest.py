457075d22ac0803bf6c7f5d890fd70c0c1cf93cc
Adrian Moennich <adrian.moennich@cern.ch>
2020-11-30 10:52:33 +0100

    VC/Zoom: Add missing file headers

--- vc_zoom/conftest.py
+++ vc_zoom/conftest.py
@@ -1,1 +1,8 @@
+# This file is part of the Indico plugins.
+# Copyright (C) 2020 CERN and ENEA
+#
+# The Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License;
+# see the LICENSE file for more details.
+
 pytest_plugins = 'indico'
