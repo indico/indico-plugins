22184a0eba835182d804c7d525d046d0dc12eed1
Adrian Moennich <adrian.moennich@cern.ch>
2021-01-27 10:56:26 +0100

    VC/Zoom: Check for deleted meeting in make-me-cohost

--- vc_zoom/indico_vc_zoom/controllers.py
+++ vc_zoom/indico_vc_zoom/controllers.py
@@ -15,8 +15,9 @@ from webargs.flaskparser import use_kwargs
 from werkzeug.exceptions import Forbidden
 
 from indico.core.db import db
+from indico.core.errors import UserValueError
 from indico.modules.vc.controllers import RHVCSystemEventBase
-from indico.modules.vc.exceptions import VCRoomError
+from indico.modules.vc.exceptions import VCRoomError, VCRoomNotFoundError
 from indico.modules.vc.models.vc_rooms import VCRoom, VCRoomStatus
 from indico.util.i18n import _
 from indico.web.rh import RH
@@ -33,6 +34,9 @@ class RHRoomAlternativeHost(RHVCSystemEventBase):
         flag_modified(self.vc_room, 'data')
         try:
             self.plugin.update_room(self.vc_room, self.event)
+        except VCRoomNotFoundError as exc:
+            db.session.rollback()
+            raise UserValueError(unicode(exc))
         except VCRoomError:
             db.session.rollback()
             raise
