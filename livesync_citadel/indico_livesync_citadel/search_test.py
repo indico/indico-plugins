0c736d1832edf6e48d25f7440e6ba57e7666bacc
Pedro <pedro.lourenco@cern.ch>
2021-03-24 20:46:06 +0000

    LiveSync/Citadel: Refactor tests

--- livesync_citadel/indico_livesync_citadel/search_test.py
+++ livesync_citadel/indico_livesync_citadel/search_test.py
@@ -1,16 +1,22 @@
+from unittest.mock import patch
+
 import pytest
 
 from indico_livesync_citadel.search import format_query
 
 
+@patch('indico_livesync_citadel.search.placeholders', {
+    'title': 'title',
+    'person': 'person'
+})
 @pytest.mark.parametrize('query,expected', [
     ('title:"my event" ola person:john ola some:yes ola',
-     'title:"my event" person:john ola ola some\\:yes ola'),
-    ('title:"my title:something"', 'title:"my title:something"'),
+     '+title:"my event" +person:john ola ola some\\:yes ola'),
+    ('title:"my title:something"', '+title:"my title:something"'),
     ('hello', 'hello'),
-    ('hey title:something', 'title:something hey'),
-    ('title:something hey', 'title:something hey'),
-    ('hey title:something hey person:john', 'title:something person:john hey hey')
+    ('hey title:something', '+title:something hey'),
+    ('title:something hey', '+title:something hey'),
+    ('hey title:something hey person:john', '+title:something +person:john hey hey')
 ])
 def test_query_placeholders(query, expected):
     assert format_query(query) == expected
