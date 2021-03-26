083d17ded602a552e7d529c5350d9ed1fdca2477
Indico Team <indico-team@cern.ch>
2021-03-26 15:51:06 +0000

    LiveSync/Citadel: Update file headers

--- livesync_citadel/indico_livesync_citadel/search_test.py
+++ livesync_citadel/indico_livesync_citadel/search_test.py
@@ -1,3 +1,10 @@
+# This file is part of the Indico plugins.
+# Copyright (C) 2002 - 2021 CERN
+#
+# The Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License;
+# see the LICENSE file for more details.
+
 from unittest.mock import patch
 
 import pytest
