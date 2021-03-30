e40c53ae4bf4888d5a9612f5dbc89c9109bf9e09
Adrian Moennich <adrian.moennich@cern.ch>
2021-03-30 14:44:07 +0200

    Citadel: Move metadata to setup.cfg

--- citadel/setup.py
+++ citadel/setup.py
@@ -5,31 +5,7 @@
 # them and/or modify them under the terms of the MIT License;
 # see the LICENSE file for more details.
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
-setup(
-    name='indico-plugin-citadel',
-    version='1.0',
-    url='https://github.com/indico/indico-plugins',
-    license='MIT',
-    author='Indico Team',
-    author_email='indico-team@cern.ch',
-    packages=find_packages(),
-    zip_safe=False,
-    include_package_data=True,
-    install_requires=[
-        'indico>=3.0-dev',
-        'indico-plugin-livesync>=1.0',
-        'tika>=1.24'
-    ],
-    entry_points={
-        'indico.plugins': {'citadel = indico_citadel.plugin:LiveSyncCitadelPlugin'}
-    },
-    classifiers=[
-        'Environment :: Plugins',
-        'Environment :: Web Environment',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.9'
-    ],
-)
+setup()
