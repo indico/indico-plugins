f52ee3184ab9c4881b0a859e6512ef8faf5cc24f
Adrian <adrian.moennich@cern.ch>
2021-05-11 17:53:18 +0200

    Themes/Legacy: Add plugin with 'nicecompact' theme (#127)
    * Add plugin (data copied from core)
    * Only apply styles to event area

---
+++ themes_legacy/indico_themes_legacy/plugin.py
@@ -0,0 +1,28 @@
+# This file is part of the Indico plugins.
+# Copyright (C) 2002 - 2021 CERN
+#
+# The Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License;
+# see the LICENSE file for more details.
+
+import os
+
+from indico.core import signals
+from indico.core.plugins import IndicoPlugin, IndicoPluginBlueprint
+
+
+class LegacyThemesPlugin(IndicoPlugin):
+    """Legacy Themes
+
+    Provides legacy event themes
+    """
+
+    def init(self):
+        super().init()
+        self.connect(signals.plugin.get_event_themes_files, self._get_themes_yaml)
+
+    def get_blueprints(self):
+        return IndicoPluginBlueprint(self.name, __name__)
+
+    def _get_themes_yaml(self, sender, **kwargs):
+        return os.path.join(self.root_path, 'themes-legacy.yaml')
