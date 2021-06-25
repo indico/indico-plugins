a9ccd18b3d1f1009055155a33723d6ed226b110b
Javier Ferrer <javier.ferrer@cern.ch>
2021-06-25 18:11:22 +0200

    VC/Zoom: Confirm making yourself alternative host (#149)

--- vc_zoom/indico_vc_zoom/client/index.js
+++ vc_zoom/indico_vc_zoom/client/index.js
@@ -5,7 +5,11 @@
 // them and/or modify them under the terms of the MIT License;
 // see the LICENSE file for more details.
 
+/* global confirmPrompt:false, $T:false */
+
 import {handleAxiosError, indicoAxios} from 'indico/utils/axios';
+
+const $t = $T.domain('vc_zoom');
 
 document.addEventListener('DOMContentLoaded', async () => {
   $('.vc-toolbar').dropdown({
@@ -15,15 +19,20 @@ document.addEventListener('DOMContentLoaded', async () => {
   });
 
   document.querySelectorAll('.vc-toolbar .action-make-host').forEach(elem => {
-    elem.addEventListener('click', async () => {
-      const killProgress = IndicoUI.Dialogs.Util.progress();
-      try {
-        await indicoAxios.post(elem.dataset.href);
-        window.location.reload();
-      } catch (error) {
-        handleAxiosError(error);
-        killProgress();
-      }
+    elem.addEventListener('click', () => {
+      confirmPrompt(
+        $t.gettext('Are you sure you want to be added as an alternative host?'),
+        $t.gettext('Make me an alternative host')
+      ).then(async () => {
+        const killProgress = IndicoUI.Dialogs.Util.progress();
+        try {
+          await indicoAxios.post(elem.dataset.href);
+          window.location.reload();
+        } catch (error) {
+          handleAxiosError(error);
+          killProgress();
+        }
+      });
     });
   });
 });
