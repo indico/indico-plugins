7bbfea353d0402669da26c18d93dcd6a58d1dcdd
Indico Team <indico-team@cern.ch>
2021-10-14 11:38:38 +0200

    Add missing file headers

--- manage-i18n.sh
+++ manage-i18n.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# This file is part of the Indico plugins.
+# Copyright (C) 2002 - 2021 CERN
+#
+# The Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License;
+# see the LICENSE file for more details.
 
 USAGE="$0 [init <locale>|extract|update <locale>|compile <locale>]"
 
