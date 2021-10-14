ccc217f4942cf88a64aa5c36778bdde7bc6ac1f7
Indico Team <indico-team@cern.ch>
2021-10-14 11:38:38 +0200

    Add missing file headers

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# This file is part of the Indico plugins.
+# Copyright (C) 2002 - 2021 CERN
+#
+# The Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License;
+# see the LICENSE file for more details.
 
 for dir in $(find -name pytest.ini -exec dirname {} \;); do
     pushd "$dir" >/dev/null
