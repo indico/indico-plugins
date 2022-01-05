7276ea793ee17fcc6eb73ae5c59fdbf3eeec2c23
Indico Team <indico-team@cern.ch>
2022-01-05 11:41:25 +0100

    Happy new year 2022 :fireworks:

--- piwik/indico_piwik/client/index.js
+++ piwik/indico_piwik/client/index.js
@@ -1,5 +1,5 @@
 // This file is part of the Indico plugins.
-// Copyright (C) 2002 - 2021 CERN
+// Copyright (C) 2002 - 2022 CERN
 //
 // The Indico plugins are free software; you can redistribute
 // them and/or modify them under the terms of the MIT License;
