05ba82ba75f7a72de3391b1d44dfda65ea366d5e
Indico Team <indico-team@cern.ch>
2024-01-02 20:51:21 +0100

    Happy new year 2024 :fireworks:

--- payment_sixpay/setup.py
+++ payment_sixpay/setup.py
@@ -1,5 +1,5 @@
 # This file is part of the Indico plugins.
-# Copyright (C) 2017 - 2023 Max Fischer, Martin Claus, CERN
+# Copyright (C) 2017 - 2024 Max Fischer, Martin Claus, CERN
 #
 # The Indico plugins are free software; you can redistribute
 # them and/or modify them under the terms of the MIT License;
