f04803e2ecec43664f3124f80b85def6fe401875
Adrian Moennich <adrian.moennich@cern.ch>
2025-03-18 12:01:00 +0100

    VC/Zoom: Fix IDs stored as strings

--- vc_zoom/indico_vc_zoom/cli.py
+++ vc_zoom/indico_vc_zoom/cli.py
@@ -5,11 +5,16 @@
 # them and/or modify them under the terms of the MIT License;
 # see the LICENSE file for more details.
 
+from operator import attrgetter
+
 import click
+from sqlalchemy.orm.attributes import flag_modified
 from terminaltables import AsciiTable
 
 from indico.cli.core import cli_group
+from indico.core.db import db
 from indico.modules.vc.models.vc_rooms import VCRoom, VCRoomStatus
+from indico.util.console import verbose_iterator
 
 
 @cli_group(name='zoom')
@@ -34,3 +39,20 @@ def meetings(status=None):
     for col in (0, 3, 4):
         table.justify_columns[col] = 'right'
     print(table.table)
+
+
+@cli.command()
+def fix_ids():
+    """Fixes Zoom IDs stored as strings.
+
+    This ensures Zoom webhooks correctly work for those meetings.
+    """
+
+    res = VCRoom.query.filter(VCRoom.type == 'zoom', db.func.jsonb_typeof(VCRoom.data['zoom_id']) == 'string').all()
+    for vcr in verbose_iterator(res, len(res), get_id=attrgetter('id'), get_title=lambda x: x.data['zoom_id'],
+                                print_total_time=True):
+        assert str(int(vcr.data['zoom_id'])) == vcr.data['zoom_id']
+        vcr.data['zoom_id'] = int(vcr.data['zoom_id'])
+        flag_modified(vcr, 'data')
+
+    db.session.commit()
