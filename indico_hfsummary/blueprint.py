2f269ba1651d6bb189d5694700fdc0461e797858
zeynep caysar <zeynepcaysar@gmail.com>
2025-07-30 17:51:34 +0200

    updates

--- indico_hfsummary/blueprint.py
+++ indico_hfsummary/blueprint.py
@@ -1,10 +1,9 @@
+'''
 from indico.core.plugins import IndicoPluginBlueprint
-
-from indico_hfsummary.plugin import IndicoHFSummaryPlugin
+from indico_hfsummary.plugin import summarize
 
 blueprint = IndicoPluginBlueprint('hfsummary', __name__, url_prefix='/plugin/hfsummary')
+blueprint.add_url_rule('/summarize', 'summarize', summarize, methods=['POST'])
 
-
-blueprint.add_url_rule('/summarize',IndicoHFSummaryPlugin, methods=['POST'])
-
+'''
 
---
+++ indico_hfsummary/controllers.py
@@ -0,0 +1,39 @@
+from flask import request, jsonify
+from indico.core.plugins import IndicoPluginBlueprint
+from indico_hfsummary.utils import clean_html_text, chunk_text, cern_qwen, build_prompt, convert_text_to_html_sections
+import time
+
+blueprint = IndicoPluginBlueprint('hfsummary', __name__, url_prefix='/plugin/hfsummary')
+
+@blueprint.route('/summarize', methods=['POST'])
+
+def summarize():
+    start_time = time.time()
+    data = request.get_json()
+    raw_html = data.get('message_text')
+    token = data.get('token')
+
+    if not raw_html or not token:
+        return jsonify({"error": "Both message_text and token are required"}), 400
+
+    cleaned_text = clean_html_text(raw_html)
+    chunks = chunk_text(cleaned_text)
+    summaries = []
+
+    for chunk in chunks:
+        prompt = build_prompt(chunk)
+        response = cern_qwen(prompt, token)
+        if response:
+            summary = response['choices'][0]['message']['content']
+            summaries.append(summary)
+        else:
+            summaries.append("[Error during summarization]")
+
+    combined_summary = "\n".join(summaries)
+    html_output = convert_text_to_html_sections(combined_summary)
+    processing_time = round(time.time() - start_time, 2)
+
+    return jsonify({
+        "summary_html": html_output,
+        "processing_time": processing_time
+    })
