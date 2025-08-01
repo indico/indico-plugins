c5df321d2fb2fa3cb177ec4f9e599c8a3850e347
zeynep caysar <zeynepcaysar@gmail.com>
2025-08-01 11:45:58 +0200

    postman post request works!!

--- indico_hfsummary/controllers.py
+++ indico_hfsummary/controllers.py
@@ -1,35 +1,58 @@
 from flask import request, jsonify
-from indico.core.plugins import IndicoPluginBlueprint
 from indico_hfsummary.utils import clean_html_text, chunk_text, cern_qwen, build_prompt, convert_text_to_html_sections
 import time
 
-def summarize():
-    start_time = time.time()
-    data = request.get_json()
-    raw_html = data.get('message_text')
-    token = data.get('token')
+from indico.web.rh import RH
 
-    if not raw_html or not token:
-        return jsonify({"error": "Both message_text and token are required"}), 400
 
-    cleaned_text = clean_html_text(raw_html)
-    chunks = chunk_text(cleaned_text)
-    summaries = []
+class Summarizer(RH):
+    CSRF_ENABLED = False # disable the protection for external api posts(postman)
 
-    for chunk in chunks:
-        prompt = build_prompt(chunk)
-        response = cern_qwen(prompt, token)
-        if response:
-            summary = response['choices'][0]['message']['content']
-            summaries.append(summary)
-        else:
-            summaries.append("[Error during summarization]")
+    def _process_POST(self):
+        start_time = time.time()
+        data = request.get_json()
+        raw_html = data.get('message_text')
+        token = data.get('token')
 
-    combined_summary = "\n".join(summaries)
-    html_output = convert_text_to_html_sections(combined_summary)
-    processing_time = round(time.time() - start_time, 2)
+        if not raw_html or not token:
+            return jsonify({"error": "Both message_text and token are required"}), 400
 
-    return jsonify({
-        "summary_html": html_output,
-        "processing_time": processing_time
-    })
+        cleaned_text = clean_html_text(raw_html)
+        chunks = chunk_text(cleaned_text)
+        summaries = []
+
+        for chunk in chunks:
+            prompt = build_prompt(chunk)
+            response = cern_qwen(prompt, token)
+            if response:
+                summary = response['choices'][0]['message']['content']
+                summaries.append(summary)
+            else:
+                summaries.append("[Error during summarization]")
+
+        combined_summary = "\n".join(summaries)
+        html_output = convert_text_to_html_sections(combined_summary)
+        processing_time = round(time.time() - start_time, 2)
+
+        return f"""
+        <html>
+        <head><title>Meeting Minutes Summary</title></head>
+        <body>
+        <h2>Meeting Minutes Summary</h2>
+        {''.join(html_output)}
+        <p><i>Processing Time: {processing_time} seconds</i></p>
+        </body>
+        </html>
+        """
+
+
+        '''
+
+        return jsonify({
+                    "summary_html": html_output,
+                    "processing_time": processing_time
+                })
+
+        '''
+        
+            
