b40f2bf4e7085631c1faeb0eb34ab0cff2ed8e9c
zeynep caysar <zeynepcaysar@gmail.com>
2025-08-01 10:05:19 +0200

    update- it doesnt throw an error

--- indico_hfsummary/plugin.py
+++ indico_hfsummary/plugin.py
@@ -1,5 +1,5 @@
 from indico.core.plugins import IndicoPlugin
-from indico_hfsummary.controllers import blueprint
+from indico_hfsummary.blueprint import blueprint
 
 class IndicoHFSummaryPlugin(IndicoPlugin):
     def init(self):
