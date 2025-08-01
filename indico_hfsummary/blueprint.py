11c6cbe1a403273e0dde620238c879393430c633
zeynep caysar <zeynepcaysar@gmail.com>
2025-08-01 17:18:49 +0200

    accessing the event minutes

--- indico_hfsummary/blueprint.py
+++ indico_hfsummary/blueprint.py
@@ -9,4 +9,6 @@ from indico_hfsummary.controllers import Summarizer
 blueprint.add_url_rule('/summarize', 'summarize', Summarizer, methods=('POST','GET'))
 
 
+from indico_hfsummary.controllers import SummarizeEvent
 
+blueprint.add_url_rule('/summarize-event/<int:event_id>', 'summarize_event', SummarizeEvent, methods=('GET',))
