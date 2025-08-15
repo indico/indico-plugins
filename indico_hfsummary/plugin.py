56b14ddd497233f80a4551bea15058dbcbe3d7cb
zeynep caysar <zeynepcaysar@gmail.com>
2025-08-15 13:35:20 +0200

    updates

--- indico_hfsummary/plugin.py
+++ indico_hfsummary/plugin.py
@@ -11,7 +11,8 @@ class IndicoHFSummaryPlugin(IndicoPlugin):
     def init(self):
         super().init()
         self.template_hook('event-manage-dropdown-after-notes-compile', self.summary_button)
-        self.inject_bundle('main.js', WPSimpleEventDisplay)
+        self.inject_bundle('main.js', WPSimpleEventDisplay) 
+        self.inject_bundle('main.css', WPSimpleEventDisplay)
 
     def get_blueprints(self):
         return blueprint
@@ -19,3 +20,4 @@ class IndicoHFSummaryPlugin(IndicoPlugin):
     def summary_button(self, event):
         print("work damn you")
         return render_plugin_template('summarize_button.html', event = event)
+    
