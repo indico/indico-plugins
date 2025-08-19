c61cece85654cc4ccf4d93ba3b56bedb9b9b658f
zeynep caysar <zeynepcaysar@gmail.com>
2025-08-19 16:33:26 +0200

    last minor updates

--- indico_hfsummary/controllers.py
+++ indico_hfsummary/controllers.py
@@ -1,5 +1,5 @@
 from flask import request, jsonify
-from indico_hfsummary.utils import clean_html_text, chunk_text, cern_qwen, build_prompt, convert_text_to_html_sections, markdown_to_html, html_to_markdown
+from indico_hfsummary.utils import chunk_text, cern_qwen, markdown_to_html, html_to_markdown
 import time
 import os 
 from indico.modules.events.controllers.base import RHEventBase
@@ -19,28 +19,22 @@ class SummarizeEvent(RHEventBase):
         event = self.event
         prompt_template = request.args.get('prompt', '').strip()
 
-        notes = get_scheduled_notes(self.event)
+        notes = get_scheduled_notes(event)
         contrib_notes_html = "\n\n".join(note.html for note in notes if hasattr(note, 'html'))
-        raw_notes = contrib_notes_html
+        meeting_notes = contrib_notes_html
 
-        if not raw_notes.strip():
-            return jsonify({"error": "No meeting notes found for this event or its contributions."}), 400
+        if not meeting_notes.strip():
+            return jsonify({"error": "No meeting notes found from this event's contributions."}), 400
 
         start_time = time.time()
-        #cleaned_text = clean_html_text(raw_notes)
-        cleaned_text= html_to_markdown(raw_notes)
+        cleaned_text= html_to_markdown(meeting_notes)
         chunks = chunk_text(cleaned_text)
         summaries = []
         token = os.getenv('CERN_SUMMARY_API_TOKEN')
 
         for chunk in chunks:
-            # use passed prompt template
             full_prompt = f"{prompt_template.strip()}\n\n{chunk}"
-            
-            print(f"*********** Prompt sent to model:\n{full_prompt}")
-
             response = cern_qwen(full_prompt, token)
-            print(f"\n*********** Response from  model:\n{response}")
             
             if response:
                 summary = response['choices'][0]['message']['content']
@@ -49,11 +43,7 @@ class SummarizeEvent(RHEventBase):
                 summaries.append("[Error during summarization]")
 
         combined_summary = "\n".join(summaries)
-        print("---------Summary ---------")
-        print(combined_summary)
         html_output = markdown_to_html(combined_summary)
-        print(f"[SummarizeEvent] Summarizing event {event.id}: {event.title}")
-        
         print(f"Summary html output:\n{html_output}")
         
         processing_time = round(time.time() - start_time, 2)
